3f36a2c33e0b0ba55cada706a0ea296d4a99fae5
Dídac Ballabriga <didac@ballabriga.com>
2022-06-04 18:11:29 +0200

    019252e

---
+++ _nuxt/static/1654359079/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fpremium\u002Ffavoritelist","\u002Finstructions","\u002Fenjoy","\u002Flogin","\u002Fpremium","\u002Fhome","\u002Fregistrer"]})
---
+++ _nuxt/static/1654359079/premium/favoritelist/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/premium/favoritelist", {data:[{}],fetch:{},mutations:[]});
